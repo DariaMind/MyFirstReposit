c0bfa95d411bf076aa1fb6a04d4bcf5aaa09c9f8
Daria Mi <mindak.da@gmail.com>
2020-11-29 13:02:18 +0200

    First commit

---
+++ core/controllers/MainController.php
@@ -0,0 +1,26 @@
+<?php
+namespace Core\Controllers;
+use Core\Views\View;
+class MainController extends Controller{
+
+    public function index()
+    {
+        $title = 'Home Page';
+        $articles = [
+        [
+            'name'=> 'Article 1',
+            'content'=> 'Text for Article 2'
+        ],
+        [
+            'name'=> 'Article 1',
+            'content'=> 'Text for Article 2'
+        ]
+        ];
+        View::render('main/index', compact('title', 'articles'));
+    }
+
+    public function contacts()
+    {
+        View::render('main/contacts');
+    }
+}
