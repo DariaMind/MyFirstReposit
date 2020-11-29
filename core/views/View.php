ba90c3606c68ca84425a7cfe4dc40435fdcff45b
Daria Mi <mindak.da@gmail.com>
2020-11-29 13:18:53 +0200

    DZ

--- core/views/View.php
+++ core/views/View.php
@@ -7,11 +7,8 @@ class View{
         http_response_code($code);
         extract($data);
         unset($data);
-
         require_once __DIR__.'/templates/header.php';
-
         require_once __DIR__.'/templates/'.$path. '.php';
-
         require_once __DIR__.'/templates/footer.php';
     }
 }
